9a7242ff9b44fde8e900ff436e4735cee1f28d7e
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-06-30 21:23:18 -0300

    criação das funções, e deixando o código mais limpo.

--- pacman.c
+++ pacman.c
@@ -1,12 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "pacman.h"
 
 char** mapa;
 int linhas;
 int colunas;
 
-int main () {
+void liberamapa() {
+    for (int i = 0; i < linhas; i++){
+        free(mapa[i]);
+    }
+    free(mapa);
+}
 
+void alocamapa() {
+    mapa = malloc(sizeof(char*) * linhas);
+    for (int i = 0; i < linhas; i++){
+        mapa[i] = malloc(sizeof(char) * (colunas+1));
+    }
+}
+
+void lemapa() {
     FILE* f;
     f = fopen("mapa.txt", "r");  
     if (f == 0) {
@@ -15,26 +29,23 @@ int main () {
     }
    
     fscanf(f, "%d %d", &linhas, &colunas);
-    printf("Linhas: %d, Colunas: %d\n", linhas, colunas);
 
-    
-    mapa = malloc(sizeof(char*) * linhas);
-    for (int i = 0; i < linhas; i++){
-        mapa[i] = malloc(sizeof(char) * (colunas + 1));
-    }
+    alocamapa();
     
     for (int i = 0; i < 5; i++){
     fscanf(f, "%s", mapa[i]);
     }
+    fclose(f);
+}
+
+int main () {
+
+    lemapa();
 
     for (int i = 0; i < 5; i++){
         printf("%s\n", mapa[i]);
     }
-    fclose(f);
 
-    for (int i = 0; i < linhas; i++){
-        free(mapa[i]);
-    }
-    free(mapa);
+    liberamapa();
 
 }
