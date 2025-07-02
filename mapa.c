dd622fc7c8f4cc666cefb3ae98c501fe11a7fda5
Samuel Lazarin <samuel.lazarin12@gmail.com>
2025-07-02 18:49:16 -0300

    Separação e compillação dos arquivos para melhor compreensão

--- mapa.c
+++ mapa.c
@@ -1,4 +1,4 @@
-#include "mapa.h";
+#include "mapa.h"
 #include <stdio.h>
 #include <stdlib.h>
 
--- mapa.h
+++ mapa.h
@@ -4,9 +4,9 @@ struct mapa {
     int colunas;
 };
 
+typedef struct mapa MAPA;
+
 void liberamapa(MAPA* m);
 void alocamapa(MAPA* m);
 void lemapa(MAPA* m);
 void imprimemapa(MAPA* m);
-
-typedef struct mapa MAPA;
